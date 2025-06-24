bb45b87ff257e43a8f6b063d41afefa2e5e23ab8
uchihasnd <sandymiranda.snd@gmail.com>
2025-06-24 17:33:02 -0600

    Start of the project, HTML with the contact form and contact table added

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+export const navbar = () => {
+  return `
+    <nav class="navbar">
+      <ul class="nav-link">
+        <li><a href="form.html" class="nav-button">New contact</a></li>
+      </ul>
+    </nav>
+  `;
+};

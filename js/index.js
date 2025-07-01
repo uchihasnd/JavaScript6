6a7232730361ec2a0f2668af111993c420faf45c
uchihasnd <sandymiranda.snd@gmail.com>
2025-07-01 17:47:07 -0600

    x-api-key added to the header to fix 401 error, unused inputs have been removed

--- js/index.js
+++ js/index.js
@@ -2,9 +2,8 @@ import { navbar } from "./navbar.js";
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("navbar-container").innerHTML = navbar();
 
-  const btnSave = document.getElementById("btn-save");
+  const loadingSpinner = document.getElementById("spinner");
   const formElement = document.getElementById("formId");
-  console.log("Test");
 
   const getContact = () => {
     const contact = {
@@ -16,44 +15,65 @@ document.addEventListener("DOMContentLoaded", function () {
       company: document.getElementById("company").value,
     };
 
-    console.log("Test");
-
-    btnSave.textContent = "Sending...";
+    loadingSpinner.style.opacity = "1";
 
     fetch("https://reqres.in/api/users?delay=4", {
       method: "POST",
       body: JSON.stringify(contact),
       headers: {
         "Content-Type": "application/json",
+        "x-api-key": "reqres-free-v1"
       },
     })
       .then((response) => response.json())
-      .then(() => window.location.href = "tableContacts.html")
       .then(() => {
-        const tableBody = document.querySelector(".table-body");
-
-        const row = document.createElement("tr");
-
-        const nameCell = document.createElement("td");
-        nameCell.textContent = `${contact.first_name} ${contact.last_name}`;
-
-        const emailCell = document.createElement("td");
-        emailCell.textContent = contact.email;
-
-        const phoneNumberCell = document.createElement("td");
-        phoneNumberCell.textContent = contact.phone_number;
-
-        const jobCompanyCell = document.createElement("td");
-        jobCompanyCell.textContent = `${contact.job_title} at ${contact.company}`;
-
-        row.appendChild(nameCell);
-        row.appendChild(emailCell);
-        row.appendChild(phoneNumberCell);
-        row.appendChild(jobCompanyCell);
-
-        tableBody.appendChild(row);
+        const storedContact = JSON.parse(localStorage.getItem("contacts")) || [];
+        storedContact.push(contact);
+        localStorage.setItem("contacts", JSON.stringify(storedContact));
+        window.location.href = "tableContacts.html";
+      })
+      .catch((error) => {
+        console.error("Error:", error);
       });
   };
+
+  const contacts = JSON.parse(localStorage.getItem("contacts"));
+
+  const tableBody = document.querySelector(".table-body");
+
+  if (tableBody) {
+    contacts.forEach((contact) => {
+      const row = document.createElement("tr");
+      row.classList.add("table-row-body");
+
+      const nameCell = document.createElement("td");
+      nameCell.classList.add("table-data-cell");
+      nameCell.textContent = `${contact.first_name} ${contact.last_name}`;
+
+      const emailCell = document.createElement("td");
+      emailCell.classList.add("table-data-cell");
+      emailCell.textContent = contact.email;
+
+      const phoneCell = document.createElement("td");
+      phoneCell.classList.add("table-data-cell");
+      phoneCell.textContent = contact.phone_number;
+
+      const jobCell = document.createElement("td");
+      jobCell.classList.add("table-data-cell");
+      if (contact.job_title && contact.company) {
+        jobCell.textContent = `${contact.job_title} at ${contact.company}`;
+      } else {
+        jobCell.textContent = `${contact.job_title || contact.company}`;
+      }
+
+      row.appendChild(nameCell);
+      row.appendChild(emailCell);
+      row.appendChild(phoneCell);
+      row.appendChild(jobCell);
+
+      tableBody.appendChild(row);
+    });
+  }
 
   if (formElement) {
     formElement.addEventListener("submit", (event) => {
